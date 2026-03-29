b8d618ca4d3f980db2db598ed39aa83a7d3b88f6
utgwkk <utagawakiki@gmail.com>
2026-03-29 19:16:35 +0900

    response rewrite proxy

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/utgwkk/20260329-dynamodb-local-terraform-test
 
 go 1.26.1
+
+require github.com/ne-sachirou/go-graceful v0.2.1
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/ne-sachirou/go-graceful"
+	"github.com/ne-sachirou/go-graceful/gracefulhttp"
+)
+
+func main() {
+	ctx := context.Background()
+
+	slog.SetDefault(
+		slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource: true,
+			Level:     slog.LevelDebug,
+		})),
+	)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		cloneReq := r.Clone(r.Context())
+		cloneReq.RequestURI = ""
+		cloneReq.URL.Scheme = "http"
+		cloneReq.URL.Host = "localhost:8000"
+
+		slog.DebugContext(
+			ctx, "attempting to request",
+			slog.String("host", cloneReq.Host),
+			slog.String("method", cloneReq.Method),
+			slog.String("path", cloneReq.URL.Path),
+			slog.String("xAmzTarget", cloneReq.Header.Get("X-Amz-Target")),
+		)
+
+		proxyResp, err := http.DefaultClient.Do(cloneReq)
+		if err != nil {
+			slog.ErrorContext(ctx, "failed to request", slog.Any("error", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer proxyResp.Body.Close()
+
+		slog.DebugContext(
+			ctx, "got an response",
+			slog.String("status", proxyResp.Status),
+		)
+
+		for k, headers := range proxyResp.Header {
+			for _, h := range headers {
+				w.Header().Add(k, h)
+			}
+		}
+		if strings.HasSuffix(cloneReq.Header.Get("X-Amz-Target"), ".DescribeTable") && proxyResp.StatusCode == http.StatusOK {
+			slog.InfoContext(ctx, "attempting rewrite response JSON")
+			data := map[string]any{}
+			if err := json.NewDecoder(proxyResp.Body).Decode(&data); err != nil {
+				slog.ErrorContext(ctx, "failed to decode JSON", slog.Any("error", err))
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			slog.DebugContext(ctx, "raw response", slog.Any("data", data))
+
+			// append dummy ProvisionedThroughput
+			data["ProvisionedThroughput"] = map[string]int{
+				"NumberOfDecreasesToday": 0,
+				"ReadCapacityUnits":      5,
+				"WriteCapacityUnits":     5,
+			}
+
+			encoded := &bytes.Buffer{}
+			if err := json.NewEncoder(encoded).Encode(data); err != nil {
+				slog.ErrorContext(ctx, "failed to encode JSON", slog.Any("error", err))
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			w.WriteHeader(proxyResp.StatusCode)
+			w.Write(encoded.Bytes())
+			slog.InfoContext(ctx, "response rewrite succeeded")
+		} else {
+			w.WriteHeader(proxyResp.StatusCode)
+			io.Copy(w, proxyResp.Body)
+		}
+	})
+
+	if err := gracefulhttp.ListenAndServe(
+		ctx,
+		":8001",
+		nil,
+		graceful.GracefulShutdownTimeout(10*time.Second),
+	); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		slog.ErrorContext(ctx, "failed to listen", slog.Any("error", err))
+	}
+}
